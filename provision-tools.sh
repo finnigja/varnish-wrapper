85147d44c9b3833fd7ba415eeaa3dc12c84f8c19
Jamie Finnigan <jamiefinnigan@gmail.com>
2016-03-27 22:15:21 -0700

    include sqlite3 in install

--- provision-tools.sh
+++ provision-tools.sh
@@ -2,6 +2,6 @@
 sudo apt-get install tcpdump nmap
 
 #add mitmproxy
-sudo apt-get install python-dev python-pip libxml2-dev libffi-dev libxslt1-dev libjpeg8-dev zlib1g-dev -y
+sudo apt-get install python-dev python-pip libxml2-dev libffi-dev libxslt1-dev libjpeg8-dev zlib1g-dev sqlite3 -y
 sudo pip install mitmproxy
 
