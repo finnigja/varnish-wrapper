0cd69cbd055184d5999d01433648fecd8745e3bd
Jamie Finnigan <jamiefinnigan@gmail.com>
2016-03-27 22:23:46 -0700

    log responses as well

--- mitmdump-logger.py
+++ mitmdump-logger.py
@@ -7,15 +7,20 @@ import uuid
 
 def start(context, argv):
     context.position = argv[1]
+    context.content_limit = -1  # can use this to limit content stored
     context.db_conn = sqlite3.connect(
         'flows.db',
         check_same_thread=False  # allow reuse across mitmproxy instances
     )
     c = context.db_conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS flows (
+    c.execute('''CREATE TABLE IF NOT EXISTS request (
                  position text, method text, scheme text, host text,
-                 port text, path text, headers text, context text,
-                 uuid text)'''
+                 port text, path text, headers blob, content blob,
+                 uuid text, time text)'''
+             )
+    c.execute('''CREATE TABLE IF NOT EXISTS response (position text,
+                 http_version text, status_code text, reason text,
+                 headers blob, content blob, uuid text, time text)'''
              )
     context.db_conn.commit()
     context.log('{0} initialized ({1})'.format(argv[0], argv[1]))
@@ -28,9 +33,24 @@ def done(context):
 def request(context, flow):
     c = context.db_conn.cursor()
     req_uuid = flow.request.headers.pop('x-mitm-uuid', str(uuid.uuid4()))
+    req_time = flow.request.headers.pop('x-mitm-time', str(flow.request.timestamp_start))
     t = (context.position, flow.request.method, flow.request.scheme,
          flow.request.host, flow.request.port, flow.request.path,
-         str(flow.request.headers), flow.request.content, req_uuid)
-    c.execute('INSERT INTO flows VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', t)
+         sqlite3.Binary(unicode(flow.request.headers)),
+         sqlite3.Binary(flow.request.get_decoded_content()), req_uuid, req_time)
+    c.execute('INSERT INTO request VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', t)
     context.db_conn.commit()
     flow.request.headers.set_all('x-mitm-uuid', (req_uuid,))
+    flow.request.headers.set_all('x-mitm-time', (req_time,))
+
+@concurrent
+def response(context, flow):
+    c = context.db_conn.cursor()
+    req_uuid = flow.request.headers.pop('x-mitm-uuid')
+    req_time = flow.request.headers.pop('x-mitm-time')
+    t = (context.position, flow.response.http_version,
+         flow.response.status_code, flow.response.reason,
+         sqlite3.Binary(unicode(flow.response.headers)),
+         sqlite3.Binary(flow.response.get_decoded_content()[0:context.content_limit]), req_uuid, req_time)
+    c.execute(u'INSERT INTO response VALUES (?, ?, ?, ?, ?, ?, ?, ?)', t)
+    context.db_conn.commit()
